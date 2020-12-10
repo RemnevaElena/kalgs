cfc2de0ff1f975da13384aaf52a45986b6c645c7
RemnevaElena <remneva.ee@edu.spbstu.ru>
2020-12-10 21:32:29 +0300

    .

--- test/lesson7/AbstractDynamicTests.kt
+++ test/lesson7/AbstractDynamicTests.kt
@@ -55,6 +55,11 @@ abstract class AbstractDynamicTests {
                 )
             )
         )
+        assertEquals(listOf(23, 34, 56, 87, 91, 98, 140, 349), longestIncreasingSubSequence
+            (listOf( 23, 76, 34, 93, 123, 21, 56, 87, 91, 12, 45, 98, 140, 12, 5, 38, 349, 65, 94,
+            45, 76, 15, 99, 100, 88, 84, 35, 88
+            )
+        ))
     }
 
     fun shortestPathOnField(shortestPathOnField: (String) -> Int) {
